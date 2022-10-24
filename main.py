8a4b05d621bd33951d7692ed8338ce569921618a
Whitney Goodey <wgoodey@gmail.com>
2022-10-24 12:51:30 -0700

    Simplified caesar_cipher() function, added ability to rerun without quitting, and added ASCII art.

--- main.py
+++ main.py
@@ -1,35 +1,46 @@
+import art
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
 
 # take a string and shift the letters forward according to the specified shift value
 def caesar_cipher(mode, text, shift):
-    new_text = ""
+    output = ""
+    if mode == "decode":
+        shift *= -1
     for char in text:
         if not char.isalpha():
-            new_text += char
+            output += char
         else:
             for pos in range(len(alphabet)):
                 if alphabet[pos] == char:
-                    if mode == "encode":
-                        new_position = (pos + shift) % len(alphabet)
-                    else:
-                        new_position = (pos - shift) % len(alphabet)
+                    new_position = (pos + shift) % len(alphabet)
+                    output += alphabet[new_position]
+    return output
 
-                    new_text += alphabet[new_position]
-    return new_text
 
-# get input from user
-direction = ""
-while direction != "encode" and direction != "decode":
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+print(art.logo)
 
-text = input("Type your message:\n").lower()
-shift = -1
-while shift < 0:
-    shift = int(input("Type the shift number:\n"))
-    if shift < 0:
-        print("Please enter a positive shift value.")
+end = False
 
-print(f"Here's the result: {caesar_cipher(direction, text, shift)}")
+while not end:
 
+    # get input from user
+    direction = ""
+    while direction != "encode" and direction != "decode":
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+
+    text = input("Type your message:\n").lower()
+    shift = 0
+    while shift == 0:
+        try:
+            shift = int(input("Type the shift number:\n"))
+        except ValueError:
+            print("Please enter an integer for the shift value.")
+
+    print(f"Here's the result: {caesar_cipher(direction, text, shift)}")
+
+    again = input('\n\nDo you want to run again? Type "yes".\n').lower()
+    if again != "yes":
+        end = True
