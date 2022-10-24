6bc376a09d6f61b41e89aefaf8c448e54ec58e22
Whitney Goodey <wgoodey@gmail.com>
2022-10-24 13:02:24 -0700

    Streamlined shift value management.

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ def caesar_cipher(mode, text, shift):
     output = ""
     if mode == "decode":
         shift *= -1
+    shift %= len(alphabet)
     for char in text:
         if not char.isalpha():
             output += char
         else:
             for pos in range(len(alphabet)):
                 if alphabet[pos] == char:
-                    new_position = (pos + shift) % len(alphabet)
+                    new_position = (pos + shift)
                     output += alphabet[new_position]
     return output
 
@@ -44,3 +45,5 @@ while not end:
     again = input('\n\nDo you want to run again? Type "yes".\n').lower()
     if again != "yes":
         end = True
+    else:
+        print("Goodbye")
